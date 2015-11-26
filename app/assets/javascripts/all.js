c8917ead9c3ff9e6bda67769e0eb540ba473e43b
Estevão Andrade <estevaomoraes1@hotmail.com>
2015-11-26 21:46:42 -0200

    filter categories

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -8,3 +8,28 @@ $(document).on('click', ".segmento", function(){
 $(document).on('click', '.close-this', function(){
 	$(".about").hide();
 })
+
+$(function(){
+	$(document).on('change', '.categories, .importance-field', function(){
+		$(this).closest('form').submit();
+	})
+
+	$(document).on('click', '.more-impor', function(){
+		var importance_val = parseInt($(".importance-field").val());
+		var importance 		 = importance_val+1;
+		if(importance_val < 5){
+			console.log(importance);
+			$(".importance-field").val(importance);
+			$(".form-filter").submit();
+		}
+	})
+	$(document).on('click', '.less-impor', function(){
+		var importance_val = parseInt($(".importance-field").val());
+		var importance = importance_val-1;
+		if(importance_val > 0){
+			console.log(importance);
+			$(".importance-field").val(importance);
+			$(".form-filter").submit();
+		}
+	})
+})
--- app/controllers/rodovias_controller.rb
+++ app/controllers/rodovias_controller.rb
@@ -3,10 +3,14 @@ class RodoviasController < ApplicationController
 		@importance 				= importance_params
 		@categories 				= ViaCaracteristicCategory.all
 		@rodovia 						= Rodovia.find(params[:id])
+		@used_categories		= params[:categories] if params[:categories]
+		@used_categories  ||= @categories.map { |e| e.id }
 
 		@via_caracteristics = @rodovia.via_caracteristics
 																	.joins(:via_caracteristic_category)
-																	.where("via_caracteristic_categorys.importance <= #{@importance}"  )
+																	.where("via_caracteristic_categorys.importance <= #{@importance}")
+																	.where("via_caracteristic_categorys.id IN (#{@used_categories.join(',')})") 
+
 		render layout: false
 	end
 
